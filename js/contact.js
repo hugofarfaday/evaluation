cfc570f080a8329dbdfe356295bda97d6e81cd74
hugofarfaday <ugolescargot0601@gmail.com>
2022-08-25 20:17:20 +0200

    contact fini

--- js/contact.js
+++ js/contact.js
@@ -4,8 +4,8 @@
  */
 document.addEventListener('DOMContentLoaded', function () {
     particleground(document.getElementById('particles'), {
-      dotColor: '#5cbdaa',
-      lineColor: '#5cbdaa'
+      dotColor: '#FFFFFF',
+      lineColor: '#FFFFFF'
     });
     var intro = document.getElementById('intro');
     intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
